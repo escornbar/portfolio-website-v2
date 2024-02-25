25e1354ca7aaa6f2ff18860b42885d24a292b8b2
Syaamil Faiq <syaamil1136@gmail.com>
2024-02-26 03:19:07 +0800

    update Hero section font sizes

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,23 +6,23 @@ export default function Hero() {
     <>
       <div className="flex flex-col md:grid md:grid-cols-2 md:justify-center md:items-center min-h-[80vh] gap-16 antialiased">
         <div className="relative flex flex-col">
-          <h2 className="text-3xl font-bold tracking-tight text-primary overflow-hidden inline-flex flex-col h-[calc(theme(fontSize.3xl)*theme(lineHeight.tight))] md:h-[calc(theme(fontSize.4xl)*theme(lineHeight.none))]">
+          <h1 className="text-4xl lg:text-6xl font-bold tracking-tight text-primary overflow-hidden inline-flex flex-col h-[calc(theme(fontSize.4xl)*theme(lineHeight.snug))] md:h-[calc(theme(fontSize.5xl)*theme(lineHeight.none))] lg:h-[calc(theme(fontSize.7xl)*theme(lineHeight.tight))]">
             <ul className="block text-left leading-tight [&_li]:block animate-text-slide-3">
               <li>software engineer</li>
               <li>ui/ux</li>
               <li>full-stack</li>
               <li aria-hidden="true">software engineer</li>
             </ul>
-          </h2>
-          <p className="mt-2">
+          </h1>
+          <p className="mt-2 lg:text-lg">
             A full-stack developer whom just graduated first-class from the best
             school in Malaysia. Had a good run with hackathons, won some, lost
             some but definitely gained a lot. Best described as a quick learner
             and result driven type of person.
           </p>
         </div>
-        <div className="relative flex flex-col flex-1">
-          <div className="z-10">
+        <div className="relative flex flex-col flex-1 justify-center items-center">
+          {/* <div className="z-10">
             <Image
               src="/syaamil.png"
               alt="syaamil faiq"
@@ -30,17 +30,25 @@ export default function Hero() {
               height={300}
               className="mx-auto rounded-3xl lg:ml-auto lg:mr-0 xl:mr-52"
             />
+          </div> */}
+          <div className="relative w-full max-w-xs h-96 rounded-[2rem] overflow-hidden z-10">
+            <Image
+              className="object-contain"
+              fill
+              src={"/syaamil.png"}
+              alt="hero-image"
+            ></Image>
           </div>
           <div className="absolute inset-0 z-0 flex flex-col justify-center gap-4 opacity-15">
-            <div className="flex justify-center leading-none text-[9rem] md:justify-start md:text-[10rem] xl:justify-end">
+            <div className="flex justify-center leading-none text-[9rem] md:justify-start lg:justify-end xl:text-[11rem]">
               Syaamil
             </div>
-            <div className="flex justify-end leading-none text-[9rem] md:text-[10rem] md:justify-start xl:justify-end">
+            <div className="flex justify-center leading-none text-[9rem] md:justify-start lg:justify-end xl:text-[11rem]">
               Faiq
             </div>
           </div>
         </div>
-        <BackgroundBeams className="z-0"/>
+        <BackgroundBeams className="z-0" />
       </div>
     </>
   );
