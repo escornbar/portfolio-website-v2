eb29079030e6c0163f17a59604ad24b0968ade5c
Syaamil Faiq <syaamil1136@gmail.com>
2024-02-26 06:01:27 +0800

    enable smooth scroll

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth">
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
