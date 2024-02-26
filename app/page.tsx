0057f9b7de25d9254e8a3fa73586feaa38034ab0
Syaamil Faiq <syaamil1136@gmail.com>
2024-02-27 03:53:16 +0800

    add light variant for dot background

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
             <Projects />
           </SectionWrapper>
           <div className="relative">
-            <div className="absolute inset-0 -z-10 h-full w-full bg-transparent bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_60%,transparent_100%)]"></div>
+            <div className="absolute inset-0 -z-10 h-full w-full bg-transparent bg-[radial-gradient(#3d61ff_1px,transparent_1px)] dark:bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px] [mask-image:radial-gradient(ellipse_40%_70%_at_50%_50%,#000_60%,transparent_100%)]"></div>
             <SectionWrapper>
               <Stack />
             </SectionWrapper>
