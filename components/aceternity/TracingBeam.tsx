f3445b2b29181d6b6f0d7517f1bf8383dfc2a3d8
Syaamil Faiq <syaamil1136@gmail.com>
2024-02-26 21:06:32 +0800

    fit tracing beam height to content

--- components/aceternity/TracingBeam.tsx
+++ components/aceternity/TracingBeam.tsx
@@ -60,7 +60,7 @@ export default function TracingBeam({
   return (
     <motion.div
       ref={ref}
-      className={cn("relative w-full max-w-4xl mx-auto h-full", className)}
+      className={cn("relative w-full max-w-4xl mx-auto h-fit", className)}
     >
       <div className="absolute -left-20 top-3">
         <motion.div
