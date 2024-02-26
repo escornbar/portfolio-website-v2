38ac986d04626b01b67bd79d7755956275764c08
Syaamil Faiq <syaamil1136@gmail.com>
2024-02-27 03:57:30 +0800

    lint

--- app/page.tsx
+++ app/page.tsx
@@ -9,18 +9,18 @@ export default function Home() {
     <>
       <div className="overflow-hidden">
         <Hero />
+        <SectionWrapper>
+          <Experience />
+        </SectionWrapper>
+        <SectionWrapper>
+          <Projects />
+        </SectionWrapper>
+        <div className="relative">
+          <div className="absolute inset-0 -z-10 h-full w-full bg-transparent bg-[radial-gradient(#3d61ff_1px,transparent_1px)] dark:bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px] [mask-image:radial-gradient(ellipse_40%_70%_at_50%_50%,#000_60%,transparent_100%)]"></div>
           <SectionWrapper>
-            <Experience />
+            <Stack />
           </SectionWrapper>
-          <SectionWrapper>
-            <Projects />
-          </SectionWrapper>
-          <div className="relative">
-            <div className="absolute inset-0 -z-10 h-full w-full bg-transparent bg-[radial-gradient(#3d61ff_1px,transparent_1px)] dark:bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px] [mask-image:radial-gradient(ellipse_40%_70%_at_50%_50%,#000_60%,transparent_100%)]"></div>
-            <SectionWrapper>
-              <Stack />
-            </SectionWrapper>
-          </div>
+        </div>
       </div>
     </>
   );
