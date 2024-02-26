cdedf5754e542735ef9c9ab2a389c542d157eb15
Syaamil Faiq <syaamil1136@gmail.com>
2024-02-27 03:47:14 +0800

    add bg for stack section

--- app/page.tsx
+++ app/page.tsx
@@ -7,19 +7,20 @@ import SectionWrapper from "@/components/custom/SectionWrapper";
 export default function Home() {
   return (
     <>
-      <div className="container mx-auto overflow-hidden">
+      <div className="overflow-hidden">
         <Hero />
-        <div className="lg:px-24">
           <SectionWrapper>
             <Experience />
           </SectionWrapper>
           <SectionWrapper>
             <Projects />
           </SectionWrapper>
-          <SectionWrapper>
-          <Stack />
-          </SectionWrapper>
-        </div>
+          <div className="relative">
+            <div className="absolute inset-0 -z-10 h-full w-full bg-transparent bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_60%,transparent_100%)]"></div>
+            <SectionWrapper>
+              <Stack />
+            </SectionWrapper>
+          </div>
       </div>
     </>
   );
