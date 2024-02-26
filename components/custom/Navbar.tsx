3bc23e388e03fcd8a75cf9354fca8db5928b3a6c
Syaamil Faiq <syaamil1136@gmail.com>
2024-02-27 04:04:36 +0800

    center navbar links in Navbar

--- components/custom/Navbar.tsx
+++ components/custom/Navbar.tsx
@@ -75,7 +75,7 @@ export default function Navbar() {
   return (
     <>
       <div className={`min-w-screen sticky top-0 z-20 backdrop-blur-lg backdrop-filter`}>
-        <div className="container mx-auto flex justify-between items-center py-6">
+        <div className="container mx-auto grid grid-cols-2 lg:grid-cols-3 items-center py-6">
           <Logo />
           <NavigationMenu className="hidden lg:flex">
             <NavigationMenuList>
@@ -96,7 +96,7 @@ export default function Navbar() {
               ))}
             </NavigationMenuList>
           </NavigationMenu>
-          <div className="flex gap-1">
+          <div className="flex gap-1 justify-self-end">
             <ThemeToggle />
             <MobileNav />
           </div>
