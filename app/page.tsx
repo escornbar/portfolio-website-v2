7f7a335684a8e2762ed27c77fa75ecaea834e20d
Syaamil Faiq <syaamil1136@gmail.com>
2024-02-27 21:38:30 +0800

    fix key warning bug

--- app/page.tsx
+++ app/page.tsx
@@ -6,22 +6,14 @@ import SectionWrapper from "@/components/custom/SectionWrapper";
 
 export default function Home() {
   return (
-    <>
-      <div className="overflow-hidden">
-        <Hero />
-        <SectionWrapper>
-          <Experience />
-        </SectionWrapper>
-        <SectionWrapper>
-          <Projects />
-        </SectionWrapper>
-        <div className="relative">
-          <div className="absolute inset-0 -z-10 h-full w-full bg-transparent bg-[radial-gradient(#3d61ff_0.1px,transparent_0.9px)] dark:bg-[radial-gradient(#e5e7eb_0.1px,transparent_0.9px)] [background-size:16px_16px] [mask-image:radial-gradient(ellipse_40%_70%_at_50%_50%,#000_60%,transparent_100%)]"></div>
-          <SectionWrapper className="mb-60">
-            <Stack />
-          </SectionWrapper>
-        </div>
+    <div className="overflow-hidden">
+      <Hero />
+      <Experience />
+      <Projects />
+      <div className="relative">
+        <div className="absolute inset-0 -z-10 h-full w-full bg-transparent bg-[radial-gradient(#3d61ff_0.1px,transparent_0.9px)] dark:bg-[radial-gradient(#e5e7eb_0.1px,transparent_0.9px)] [background-size:16px_16px] [mask-image:radial-gradient(ellipse_40%_70%_at_50%_50%,#000_60%,transparent_100%)]"></div>
+        <Stack />
       </div>
-    </>
+    </div>
   );
 }
